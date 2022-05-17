89b454c266db0969ba5fa61c299fd8a4f8350825
Obi b. Uzozie <uzozie.obi@icloud.com>
2022-05-17 13:19:08 +0800

    adding layout

--- FirstBlazorApp/Data/Grocery.cs
+++ FirstBlazorApp/Data/Grocery.cs
@@ -1,8 +1,19 @@
-﻿namespace FirstBlazorApp.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace FirstBlazorApp.Data
 {
     public class Grocery
     {
+        [Required]
+        [StringLength(15, ErrorMessage = "Name should be less than 15 characters. ")]
+
+
         public string Name { get; set; }
+
+        [Required]
+        [Range(1, 10000, ErrorMessage = "Valid Price Range is (1-10000)")]
+
 
         public float  Price { get; set; }
     }
